3a628d701ad13e7a722c276006eb40b395dfc71b
Mahmoud Al Nakawa <mahmoudaymannkawa@gmail.com>
2025-04-13 22:40:11 -0400

    initial commit

---
+++ src/main/java/sheridan/adham/a4_webservices_adham/A4WebServicesAdhamApplication.java
@@ -0,0 +1,13 @@
+package sheridan.adham.a4_webservices_adham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4WebServicesAdhamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A4WebServicesAdhamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/adham/a4_webservices_adham/A4WebServicesAdhamApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.adham.a4_webservices_adham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A4WebServicesAdhamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
